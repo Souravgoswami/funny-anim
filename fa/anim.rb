8c3194d11a887d22e2b47f2a71bd3ea54f3a1aa8
Sourav Goswami <37468605+Souravgoswami@users.noreply.github.com>
2019-03-02 17:02:42 +0530

    Update anim.rb

--- fa/anim.rb
+++ fa/anim.rb
@@ -1,19 +1,20 @@
 #!/usr/bin/ruby -w
 require 'io/console'
 
-def colourize(str, colour=[92, 129, 164, 198, 203, 208, 184, 154], width=0)
-	final_str = ''
+def colourize(str, colour=[92, 129, 164, 198, 203, 208, 184, 154])
 	colour = colour.to_a if colour.is_a?(Range)
 
+	final_str = ''
 	str.each_line do |l|
-		final_str.concat(' ' * (width / 2))
 		index, colour_length = 0, colour.length
-		div = l.length/colour_length
+		div, i = l.length/colour_length, 0
 		div = 1 if div == 0
 
-		l.each_char.with_index do |c, i|
-			index += 1 if (i % div == 0) && (index < colour_length && c != ' ') && (i != 0 || i != 1)
-			final_str.concat("\e[38;5;#{colour[index]}m#{c}")
+		while i < l.length do
+			ch = l[i]
+			index += 1 if (i % div == 0) && (index < colour_length && ch != ' ') && (i != 0 || i != 1)
+			final_str.concat("\e[38;5;#{colour[index]}m#{ch}")
+			i += 1
 		end
 	end
 	final_str + "\e[0m"
@@ -26,21 +27,14 @@ def printemo(emo=':)', message='::', colour=[92, 129, 164, 198, 203, 208, 184, 1
 end
 
 def main
-	if ARGV[0]
-		dance = ARGF.read
-	else
-		puts colourize("No file provided. Usage:\n\t#{__FILE__} a_text_file.txt\n\n")
-	end
+	ARGV[0] ? (content = ARGF.read) : (puts colourize("No file provided. Usage:\n\t#{__FILE__} a_text_file.txt\n\n"))
 
-	anim = %w(| / - \\)
+	anim, val = %w(| / - \\), ''
 
-	dance_colour = [92, 129, 164, 198, 203, 208, 184, 154]
-	anim_colour = (70..75).to_a
-
+	anim_colour = [63, 33, 39, 44, 49, 83, 118]
+	content_colour = [92, 129, 164, 198, 203, 208, 184, 154]
 	equal_colour = [40,41,42,43,211, 210, 209, 208]
-	equal_colour2 = equal_colour.dup.reverse
-
-	val = ''
+	equal_colour2 = equal_colour.dup
 
 	loop do
 		anim.each do |x|
@@ -50,16 +44,16 @@ def main
 			val.concat(
 				<<~EOF
 					\e[2J\e[H\e[3J
-					#{(height/4).times.map { |i| colourize(x * width, anim_colour.rotate!) }.join}
-					#{colourize('=' * (width - 1) , equal_colour.rotate!)}
-					#{colourize(dance, dance_colour.rotate!, (width / 2))}
-					#{colourize('=' * (width - 1), equal_colour2.rotate!(-1))}
-					#{(height/4).times.map { |i| colourize(x * width, anim_colour.rotate!) }.join}
+					#{(height/3).times.map { colourize(x * width, anim_colour.rotate!) }.join}
+					#{colourize('=' * width, equal_colour.rotate!)}
+					#{colourize(content, content_colour.rotate!)}
+					#{colourize('=' * width, equal_colour2.rotate!(-1))}
+					#{(height/3).times.map { colourize(x * width, anim_colour.rotate!) }.join}
 				EOF
 			)
 
 			print val
-			sleep 0.13
+			sleep 0.1
 		end
 	end
 end
